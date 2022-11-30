7144b75ce40c5e2ce0b78fa793dcf4b5bb574008
Maria pereira <dudsp2006@gmail.com>
2022-11-30 13:13:55 -0300

    acrescentados mais 2 exercicios

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+a = int(input("digite um numero inteiro: "))
+b = float(input("digite um numero nao inteiro: "))
+r = (a*2 + b*5)*1/2
+print (r)
+x = (r*1/5) + b*3
+print(x)
---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+r = float(input("digite o raio: "))
+d = r*2
+p = 3.14
+comprimento = 2.0*p*r
+área = p*(r**2)
+print(d)
+print(comprimento)
+print(área)
